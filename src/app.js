68a852991e40cb80b49c5a69033ec6c18f267f80
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-05-08 14:39:25 -0500

    Setup handlebars partials and create basic header partial.
    Added support for handlebars partials.
    Created a basic header partial that include head and shared body content.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 const path = require('path');
 
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const webrootPath = path.join(__dirname, '..');
-const viewsPath = path.join(webrootPath, '/templates/views');
 const publicPath = path.join(webrootPath, '/public');
+const viewsPath = path.join(webrootPath, '/templates/views');
+const partialsPath = path.join(webrootPath, '/templates/partials');
 
 app.use(express.static(publicPath));
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 
+hbs.registerPartials(partialsPath);
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    pageTitle: 'Weather'
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
