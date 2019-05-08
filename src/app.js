53c5bfeafd9914dc2954217000252441556276e9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-05-08 14:18:00 -0500

    Add support for handelbars (hbs) and get basic index page to render.
    Added handlebars to the project and setup a custom path for the views folder.
    Rendred a basic index page at the root route.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const webrootPath = path.join(__dirname, '..');
+const viewsPath = path.join(webrootPath, '/templates/views');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
