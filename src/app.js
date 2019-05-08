830655a652cfc0efb590f60c06ba0c6fba7aebff
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-05-08 14:24:48 -0500

    Configure express to serve static assets.
    Setup express to serve static assets from the 'public' directory.
    Created a css and js file and confirmed they were loading.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 const webrootPath = path.join(__dirname, '..');
 const viewsPath = path.join(webrootPath, '/templates/views');
+const publicPath = path.join(webrootPath, '/public');
+
+app.use(express.static(publicPath));
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
